093e55eb2af059cbfac27fa6e92ba0c148e7c640
arora-gaurav <gauravcs25@gmail.com>
2020-08-13 18:07:05 +0530

    Check Authentication on creating post and display posts & related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,22 @@
 const User = require("../models/user");
+const Post = require('../models/post');
 
 module.exports.home = function(req, res){
     // res.cookie('something', 'RAM');
-    return res.render('home', {
-        title: 'Codial Project'
-    })
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: 'Codial | Home',
+    //         posts: posts
+    //     });
+    // });
+
+    //Populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: 'Codial | Home',
+            posts: posts
+        });
+    });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 console.log('Router Loaded');
 
 const homeController = require('../controllers/home_controller');
+const { route } = require('./posts');
 router.get('/', homeController.home);
 
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 module.exports = router;
