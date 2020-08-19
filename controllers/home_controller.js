c6fbe055f79fe142f3d5a5303e0e79e41824fc22
arora-gaurav <gauravcs25@gmail.com>
2020-08-19 18:25:05 +0530

    created and deleted post dynamically

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,22 +1,63 @@
 const User = require("../models/user");
 const Post = require('../models/post');
 
-module.exports.home = function(req, res){
-    // res.cookie('something', 'RAM');
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: 'Codial | Home',
-    //         posts: posts
-    //     });
-    // });
-
-    //Populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts){
-        return res.render('home', {
-            title: 'Codial | Home',
-            posts: posts
-        });
+module.exports.home = async function(req, res){
+    try{
+        //Populate the user of each post
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }   
     });
+    let users = await User.find({});  
+    return res.render('home', {
+        title: 'Codial | Home',
+        posts: posts,
+        all_users: users
+    });
+    } catch(err){
+        console.log('Error', err);
+        return;
+    }
 }
 
+// module.exports.home = function(req, res){
+//     // res.cookie('something', 'RAM');
+
+//     // Post.find({}, function(err, posts){
+//     //     return res.render('home', {
+//     //         title: 'Codial | Home',
+//     //         posts: posts
+//     //     });
+//     // });
+
+//     //Populate the user of each post
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path: 'comments',
+//         populate: {
+//             path: 'user'
+//         }   
+//     })
+//     .exec(function(err, posts){
+//         User.find({}, function(err,users){
+//             return res.render('home', {
+//                 title: 'Codial | Home',
+//                 posts: posts,
+//                 all_users: users
+//             });
+//         })    
+//     });
+// }
+
+
+// Using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+// posts.then();
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,14 +3,29 @@ const User = require('../models/user');
  const { user } = require('../routes');
 
 module.exports.profile = function(req, res){
-    return res.render('users_profile', {
-        title: 'Profile Page'
+    User.findById(req.params.id, function(err,user){
+        return res.render('users_profile', {
+            title: 'Profile Page',  
+            profile_user: user
+        })
     })
+    
+}
+
+module.exports.update = function(req, res){
+    if(req.params.id ==  req.user.id){
+        // Simply pass req.body if it has same name and email in user also, but since i mistakenly used user instead of name, so write explicitly
+         User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        })
+    } else {
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 module.exports.sign_in = function(req, res){
     if(req.isAuthenticated()){
-        return res.redirect('/users/profile');
+        return res.redirect('back'); 
     }
     return res.render('user_sign_in', {
         title: 'Codial | Sign In'
@@ -53,10 +68,13 @@ module.exports.create = function(req,res){
 
 // Sign-in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in Successfully');
+
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
+    req.flash('success', 'You have logged out');
     return res.redirect('/');
 }
